3a3f5655949de21c556277d715903fa370e55547
Dan Flanagan <dan.flanagan93@gmail.com>
2020-01-22 22:12:16 -0500

    Create upper_lower.py

---
+++ upper_lower.py
@@ -0,0 +1,48 @@
+def stringManipulator(a):
+    new_string = ""
+    counter = 0
+    for x in a:
+        if counter % 2 == 0:
+            new_string += x.upper()
+        else:
+            new_string += x.lower()
+        counter += 1
+    print(new_string)
+    return new_string
+
+
+
+def indexedForLoop(a):
+    new_string = ""
+    for index in range(len(a)):
+        if index % 2 == 0:
+            new_string += a[index].upper()
+        else:
+            new_string += a[index].lower()
+    print(new_string)
+    return new_string
+
+
+def evenLessCode(a):
+    new_string = ""
+    for index in range(len(a)):
+        if index % 2 == 0: new_string += a[index].upper()
+        else: new_string += a[index].lower()
+    print(new_string)
+    return new_string
+
+
+def epsilonIsntSupportedInPythonAndThatSucks(a):
+    new_string = ""
+    for index in range(len(a)):
+        #this isn't legal in python and that sucks but it would be so clean
+        #new_string += (index % 2 == 0) ? a[index].upper : a[index].lower()
+        pass
+    print(new_string)
+    return new_string
+
+
+
+stringManipulator("hello")
+indexedForLoop("hello")
+evenLessCode("hello")
